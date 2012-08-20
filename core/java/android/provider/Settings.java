4e8bc9fdc960e6f531215c435b05e1cb7f410a9d
Konsta <konsta09@gmail.com>
2012-08-20 10:39:57 +0300

    framework: Volume key cursor control (1/2)
    This feature is moved to framework so it also works with third
    party keyboards.
    
    Change-Id: I8e20240e7bee5351ab20bb3d701eb95a5fd3e112
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5018,6 +5018,15 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5136,6 +5145,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(EGG_MODE);
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
+            PRIVATE_SETTINGS.add(VOLUME_KEY_CURSOR_CONTROL);
         }
 
         /**
