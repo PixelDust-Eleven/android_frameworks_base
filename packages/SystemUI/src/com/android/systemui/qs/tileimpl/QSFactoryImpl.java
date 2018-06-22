b8dfde843f1f088f7455e956816d352012ef3fba
ezio84 <brabus84@gmail.com>
2018-06-22 17:28:16 +0200

    Add dynamic Immersive Mode tile
    on top of existing aosp policy control code
    
    - immersive statusbar
    - immersive navbar
    - full immersive
    
    quicksearchbox apk is blacklisted to avoid battery drain / unlock lag
    
    - tap to cycle modes
    - long press to enable the selected immersive mode
    - if the current immersive mode is enabled, long pressing again the tile
    will disable immersive so no need to cycle the tile to go to the "off"
    position
    - tile icon will be set to active or unactive state according to the
    current mode
    
    Change-Id: I1747b7f4077247014ca5e2a160712354df6e023e
    
    Immersive tile: more cleanup
    
    no need to have a specific "off" state because we already allow to
    disable immersive longpressing on one of the other states when
    they are active
    
    Change-Id: I524a8927a1e9f89244fccefabc5dd1926fed6608
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
+import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
@@ -88,6 +89,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SyncTile> mSyncTileProvider;
     private final Provider<AlwaysOnDisplayTile> mAlwaysOnDisplayTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
+    private final Provider<ImmersiveTile> mImmersiveTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
 
@@ -117,7 +119,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<HeadsUpTile> headsUpTileProvider,
             Provider<SyncTile> syncTileProvider,
             Provider<AlwaysOnDisplayTile> alwaysOnDisplayTileProvider,
-            Provider<CaffeineTile> caffeineTileProvider) {
+            Provider<CaffeineTile> caffeineTileProvider,
+            Provider<ImmersiveTile> immersiveTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
@@ -144,6 +147,7 @@ public class QSFactoryImpl implements QSFactory {
         mSyncTileProvider = syncTileProvider;
         mAlwaysOnDisplayTileProvider = alwaysOnDisplayTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
+        mImmersiveTileProvider = immersiveTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -205,6 +209,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mAlwaysOnDisplayTileProvider.get();
             case "caffeine":
                 return mCaffeineTileProvider.get();
+            case "immersive":
+                return mImmersiveTileProvider.get();
         }
 
         // Custom tiles
