7d47aa6974648fbfd382e04dd65901f5730a2b23
Lucchetto <lucchetto.tie@live.com>
2018-12-16 13:37:49 +0100

    Tint active volume bar with gradient
    Bootleggers (@ElDainosor) edit: Let's make it toggleable instead of enforcing it because we don't use gradients all the time
    
    Change-Id: I6528a1d06daf66c3b5f83fb968f618d886495d1f
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -164,6 +164,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean mConfigChanged = false;
     private boolean mIsAnimatingDismiss = false;
     private boolean mHasSeenODICaptionsTooltip;
+    private boolean mEnableVolumePanelTint;
     private ViewStub mODICaptionsTooltipViewStub;
     private View mODICaptionsTooltipView = null;
 
@@ -1189,10 +1190,12 @@ public class VolumeDialogImpl implements VolumeDialog,
         final int alpha = useActiveColoring
                 ? Color.alpha(tint.getDefaultColor())
                 : getAlphaAttr(android.R.attr.secondaryContentAlpha);
-        if (tint == row.cachedTint) return;
-        row.slider.setProgressTintList(tint);
+        boolean mEnableVolumePanelTint = mContext.getResources().getBoolean(R.bool.config_enableVolumePanelTint);
+        final ColorStateList progressTint = useActiveColoring ? null : tint;
+        if (tint == row.cachedTint && mExpanded) return;
+        row.slider.setProgressTintList(mEnableVolumePanelTint ? tint : progressTint);
         row.slider.setThumbTintList(tint);
-        row.slider.setProgressBackgroundTintList(tint);
+        if (!mEnableVolumePanelTint) row.slider.setProgressBackgroundTintList(tint);
         row.slider.setAlpha(((float) alpha) / 255);
         row.icon.setImageTintList(tint);
         row.icon.setImageAlpha(alpha);
