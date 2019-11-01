7d17500b57a13dfc63079f8391d052bb957907f5
ezio84 <brabus84@gmail.com>
2019-11-01 21:07:00 +0100

    Enable dev settings by default
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2591,8 +2591,8 @@ class DatabaseHelper extends SQLiteOpenHelper {
             }
             loadSetting(stmt, Settings.Global.LID_BEHAVIOR, defaultLidBehavior);
 
-            loadIntegerSetting(stmt, Global.DEVELOPMENT_SETTINGS_ENABLED,
-                    R.integer.def_development_settings);
+            loadIntegerSetting(stmt, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED,
+                    R.integer.def_enable_dev_settings);
 
             /*
              * IMPORTANT: Do not add any more upgrade steps here as the global,
