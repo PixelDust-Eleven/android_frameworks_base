75d7c4b54a204fa08794df9265bea926cabd3d6e
ezio84 <brabus84@gmail.com>
2019-11-02 10:23:25 +0100

    Allow to choose lockscreen or Ambient on wake gestures [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    PS: we could merge the "else if (isWakeLockScreen)" case in the
    longpress/wakegestures one considering it also triggers
    a requestPulse, but let's keep it as it's now just to avoid
    future merge conflicts there
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4830,6 +4830,13 @@ public final class Settings {
          */
         public static final String MULTI_AUDIO_FOCUS_ENABLED = "multi_audio_focus_enabled";
 
+        /** Whether to show ambient or lockscreen if AoD is disabled
+         * and we do a wake gesture like lift to wake or double tap
+         *
+         * @hide
+         */
+        public static final String AMBIENT_WAKE_GESTURES = "ambient_wake_gestures";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
