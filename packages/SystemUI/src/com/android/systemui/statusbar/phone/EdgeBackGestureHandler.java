77db3ac9645690069967f2a5f2ec6bb0e3d73bc2
Nico <nicorg2529@gmail.com>
2020-03-10 21:53:40 +0100

    Extended swipe: show a double arrow when almost triggered
    abcdu: adapted to our code
    Change-Id: If8905cb231e5ea0e993a590ce7f02669b46b8136
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -280,6 +280,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         mIsExtendedSwipe = mGestureNavigationSettingsObserver.getIsExtendedSwipe();
         mLeftVerticalSwipeAction = mGestureNavigationSettingsObserver.getLeftLSwipeAction();
         mRightVerticalSwipeAction = mGestureNavigationSettingsObserver.getRightLSwipeAction();
+        if (mEdgeBackPlugin != null) {
+            mEdgeBackPlugin.setLongSwipeEnabled(mIsExtendedSwipe);
+        }
 
         final DisplayMetrics dm = res.getDisplayMetrics();
         final float defaultGestureHeight = res.getDimension(
@@ -430,6 +433,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
         mEdgeBackPlugin = edgeBackPlugin;
         mEdgeBackPlugin.setBackCallback(mBackCallback);
         mEdgeBackPlugin.setLayoutParams(createLayoutParams());
+        mEdgeBackPlugin.setLongSwipeEnabled(mIsExtendedSwipe);
         updateDisplaySize();
     }
 
