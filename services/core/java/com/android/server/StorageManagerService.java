ece9c0c936f23a92393df040d6cbd943286c7e15
spezi77 <spezi7713@gmx.net>
2020-10-24 09:19:45 +0200

    Revert "Fix bug Device that can't support adoptable storage cannot read the sdcard."
    This reverts commit 030d85d09924c33f14a1ebef03d6e7cd220c2e18.

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -1530,8 +1530,6 @@ class StorageManagerService extends IStorageManager.Stub
                 vol.mountFlags |= VolumeInfo.MOUNT_FLAG_VISIBLE;
             } else if (vol.disk.isSd()) {
                 vol.mountFlags |= VolumeInfo.MOUNT_FLAG_VISIBLE;
-            } else if (vol.disk.isSd()) {
-                vol.mountFlags |= VolumeInfo.MOUNT_FLAG_VISIBLE;
             }
 
             vol.mountUserId = mCurrentUserId;
