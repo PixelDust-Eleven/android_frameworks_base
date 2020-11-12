be141aebeb062a9d03526e88bdb8b673da5eea12
ezio84 <brabus84@gmail.com>
2020-11-12 22:06:29 +0100

    PixeldustUtils: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    - Update method names in PWM
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
+++ core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
@@ -397,7 +397,7 @@ public class PixeldustUtils {
             IStatusBarService service = getStatusBarService();
             if (service != null) {
                 try {
-                    service.expandSettingsPanel(null);
+                    service.toggleSettingsPanel();
                 } catch (RemoteException e) {}
             }
         }
