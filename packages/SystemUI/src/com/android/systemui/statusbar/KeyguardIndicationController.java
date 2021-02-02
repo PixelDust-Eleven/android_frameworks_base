47086ad8a7eec534ae66339be512487056523aa7
spezi77 <spezi7713@gmx.net>
2021-02-02 10:21:19 +0100

    Ambient music: Move indication view when the charger state has changed.
    This allows the indication view to be repositioned when a charger is plugged in.

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -59,6 +59,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.settingslib.Utils;
 import com.android.settingslib.fuelgauge.BatteryStatus;
+import com.android.systemui.ambientmusic.AmbientIndicationContainer;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.broadcast.BroadcastDispatcher;
@@ -114,6 +115,7 @@ public class KeyguardIndicationController implements StateListener,
     private final DockManager mDockManager;
     private final DevicePolicyManager mDevicePolicyManager;
     private final UserManager mUserManager;
+    private AmbientIndicationContainer mAmbientIndicationContainer;
 
     private BroadcastReceiver mBroadcastReceiver;
     private LockscreenLockIconController mLockIconController;
@@ -595,6 +597,10 @@ public class KeyguardIndicationController implements StateListener,
         return mChargingIndication > 0 && mPowerPluggedIn;
     }
 
+    public void bindAmbientIndicationContainer(AmbientIndicationContainer ambientIndicationContainer) {
+        mAmbientIndicationContainer = ambientIndicationContainer;
+    }
+
     // animates textView - textView moves up and bounces down
     private void animateText(KeyguardIndicationTextView textView, String indication) {
         int yTranslation = mContext.getResources().getInteger(
@@ -871,6 +877,9 @@ public class KeyguardIndicationController implements StateListener,
                 } else if (wasPluggedIn && !mPowerPluggedIn) {
                     hideTransientIndication();
                 }
+            }
+            if (mAmbientIndicationContainer != null) {
+                mAmbientIndicationContainer.updatePosition();
             }
         }
 
