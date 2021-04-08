0ce3495c99f1619d6b00fd77272f4bffb9a516f5
Laker <lakerman1987@gmail.com>
2021-04-08 17:08:59 +0300

    SystemUI: Redesign screenshot menu buttons
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -1097,7 +1097,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         ScreenshotActionChip shareChip = (ScreenshotActionChip) inflater.inflate(
                 R.layout.global_screenshot_action_chip, mActionsView, false);
-        shareChip.setText(imageData.shareAction.title);
         shareChip.setIcon(imageData.shareAction.getIcon(), true);
         shareChip.setPendingIntent(imageData.shareAction.actionIntent, () -> {
             mUiEventLogger.log(ScreenshotEvent.SCREENSHOT_SHARE_TAPPED);
@@ -1109,7 +1108,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         ScreenshotActionChip editChip = (ScreenshotActionChip) inflater.inflate(
                 R.layout.global_screenshot_action_chip, mActionsView, false);
-        editChip.setText(imageData.editAction.title);
         editChip.setIcon(imageData.editAction.getIcon(), true);
         editChip.setPendingIntent(imageData.editAction.actionIntent, () -> {
             mUiEventLogger.log(ScreenshotEvent.SCREENSHOT_EDIT_TAPPED);
@@ -1121,7 +1119,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         ScreenshotActionChip deleteChip = (ScreenshotActionChip) inflater.inflate(
                 R.layout.global_screenshot_action_chip, mActionsView, false);
-        deleteChip.setText(imageData.deleteAction.title);
         deleteChip.setIcon(imageData.deleteAction.getIcon(), true);
         deleteChip.setPendingIntent(imageData.deleteAction.actionIntent, () -> {
             mUiEventLogger.log(ScreenshotEvent.SCREENSHOT_DELETE_TAPPED);
@@ -1134,7 +1131,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
         // Scrolling Screenshot
         ScreenshotActionChip scrollChip = (ScreenshotActionChip) inflater.inflate(
                 R.layout.global_screenshot_action_chip, mActionsView, false);
-        scrollChip.setText(imageData.scrollAction.title);
         scrollChip.setIcon(imageData.scrollAction.getIcon(), true);
         scrollChip.setPendingIntent(imageData.scrollAction.actionIntent, () -> {
             mUiEventLogger.log(ScreenshotEvent.SCREENSHOT_SCROLL_TAPPED);
@@ -1161,7 +1157,6 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                     R.layout.global_screenshot_action_chip, mActionsView, false);
             Toast scrollNotImplemented = Toast.makeText(
                     mContext, "Not implemented", Toast.LENGTH_SHORT);
-            scrollChip.setText("Extend"); // TODO: add resource and translate
             scrollChip.setIcon(
                     Icon.createWithResource(mContext, R.drawable.ic_arrow_downward), true);
             scrollChip.setOnClickListener(v -> {
