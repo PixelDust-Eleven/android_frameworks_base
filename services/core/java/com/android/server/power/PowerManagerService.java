6383b7182a75bfd3cd6ad5c526558cde21a8b1bb
spezi77 <spezi7713@gmx.net>
2021-09-17 12:57:58 +0200

    Revert bad "charger" commits.
    Revert "Fixup charger sound commit."
    Revert "Do not show charging animation when disconnecting"
    Revert "base: Use wireless charging animation for wired charging too"
    Revert "Fixup! Battery and charger sound"

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -2135,9 +2135,7 @@ public final class PowerManagerService extends SystemService
                 if (mBootCompleted) {
                     if (mIsPowered && !BatteryManager.isPlugWired(oldPlugType)
                             && BatteryManager.isPlugWired(mPlugType)) {
-                        mNotifier.onWiredChargingStarted(mUserId, mBatteryLevel);
-                    } else if (wasPowered && !mIsPowered) {
-                        mNotifier.onWiredChargingDisconnected(mUserId);
+                        mNotifier.onWiredChargingStarted(mUserId);
                     } else if (dockedOnWirelessCharger) {
                         mNotifier.onWirelessChargingStarted(mBatteryLevel, mUserId);
                     }
